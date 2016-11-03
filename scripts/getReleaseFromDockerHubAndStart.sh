45fcdc7a6817b36e97cfe11047c7243d0ff5d91d
Felix <xilef.trebor@gmail.com>
2016-11-02 21:38:04 -0400

    fix docker stop

--- scripts/getReleaseFromDockerHubAndStart.sh
+++ scripts/getReleaseFromDockerHubAndStart.sh
@@ -3,6 +3,6 @@ set –ev
 
 docker pull leonidas183/webapp:release
 
-docker rm $(docker stop -t 1 $(docker ps -q --filter="ancestor=leonidas183/webapp:release"))
+docker rm -f $(docker stop -t 5 $(docker ps -a -q --filter="ancestor=leonidas183/webapp:release"))
 
 docker run -itd -p 5050:5000 webapp:release
